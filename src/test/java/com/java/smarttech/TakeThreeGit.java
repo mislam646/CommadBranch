2d5712afb950b3b04a56f6eb5d4f03140049db87
mislam646 <mislam646@outlook.com>
2021-07-03 17:26:30 -0400

    Message new

---
+++ src/test/java/com/java/smarttech/TakeThreeGit.java
@@ -0,0 +1,10 @@
+package com.java.smarttech;
+
+public class TakeThreeGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi git");
+	}
+
+}
